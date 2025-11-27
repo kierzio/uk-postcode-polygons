660f1d1d54a0524731a0ec688d63a3d913508ca3
Peter Johnson <738069+missinglink@users.noreply.github.com>
2025-11-27 19:40:47 +0100

    updated data for 2025

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@ mkdir -p kml geojson;
 
 URL="https://en.wikipedia.org/w/index.php";
 BASEURL="$URL?title=List_of_postcode_districts_in_the_United_Kingdom&printable=yes";
+USER_AGENT="PostcodeDataBot/1.0 (https://github.com/missinglink/uk-postcode-polygons)";
 
-wget -qO- $BASEURL | sed -n 's/.*href="\/wiki\/\([A-Z]*_postcode_area\).*/\1/p' | sort | uniq | while read area;
+curl -A "$USER_AGENT" -L -s "$BASEURL" | sed -n 's/.*href="\/wiki\/\([A-Z]*_postcode_area\).*/\1/p' | sort | uniq | while read area;
   do
     if [[ $area =~ ^BF|BX|GIR ]]; then
       echo "Skipping non-geographic postcode area ${area} ..."
@@ -15,11 +16,12 @@ wget -qO- $BASEURL | sed -n 's/.*href="\/wiki\/\([A-Z]*_postcode_area\).*/\1/p' 
     else
       echo "$area";
       kmlfile="kml/${area%_*_*}.kml"
-      wget -qO- "$URL?title=Template:Attached_KML/$area&action=raw" > "${kmlfile}"
-      if [[ -s "${kmlfile}" ]]; then
+      content=$(curl -A "$USER_AGENT" -L -s "$URL?title=Template:Attached_KML/$area&action=raw")
+      if [[ -n "$content" ]] && echo "$content" | head -n 1 | grep -q '<?xml'; then
+        printf "%s" "$content" > "${kmlfile}"
         npx togeojson "${kmlfile}" | npx geojson-rewind > "geojson/${area%_*_*}.geojson";
       else
-        echo "Skipping empty postcode area ${area} ...."
+        echo "Skipping invalid or missing KML for ${area} ...."
       fi
     fi
   done;
